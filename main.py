4d2137fc1025ccdcefb42a536571b1b15e2f7dd1
max <pingwinno@gmail.com>
2025-02-13 13:38:30 +0200

    Add user filtering

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 import asyncio
+import json
 import logging
 import os
-import re
 import sqlite3
 import time
 from threading import Thread
 
 import docker
 from telegram import Update, Bot
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters
 
 # Load environment variables
 bot_token = os.environ['APIKEY']
-container_name = os.environ.get('CONTAINER_NAME')
+container_name = os.environ['CONTAINER_NAME']
+chat_list = json.loads(os.environ['CHAT_LIST'])
 
 # Ensure the database folder exists
 if not os.path.exists("db"):
@@ -32,13 +33,19 @@ delete_chat = "DELETE FROM chat_settings WHERE chat_id = ?;"
 # Configure logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
+
+async def restrict(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    logging.info(f"User {update.effective_user.id} from chat {update.effective_chat.id} sends {update.message.text}")
+
+
 # === TELEGRAM COMMAND HANDLERS === #
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     logging.info("Received /start command.")
     chat_id = update.message.chat_id
     settings_cur.execute(add_chat, [chat_id, False])
     settings_con.commit()
-    await context.bot.send_message(chat_id=chat_id, text="Chat added. Type /enable_messages to receive Factorio messages.")
+    await context.bot.send_message(chat_id=chat_id,
+                                   text="Chat added. Type /enable_messages to receive Factorio messages.")
 
 
 async def stop(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -68,12 +75,10 @@ async def disable_messages(update: Update, context: ContextTypes.DEFAULT_TYPE):
 # === CONTINUOUS LOG FILE MONITORING === #
 def monitor_logs() -> None:
     logging.info("Starting log monitoring...")
-    bot = Bot(token=bot_token)
     try:
         container = client.containers.get(container_name)
 
         logs = container.logs(stream=True, follow=True, since=int(time.time()))
-
         for log in logs:
             line = log.decode("utf-8").strip()
             logging.info(f"Log line: {line}")
@@ -85,10 +90,11 @@ def monitor_logs() -> None:
                 message_text = line.split("[LEAVE]")[1].strip()
 
             if message_text:
+                bot = Bot(token=bot_token)
                 logging.info(f"Send message: {message_text}")
                 chats = settings_cur.execute(get_chats).fetchall()
                 for chat in chats:
-                   asyncio.run(bot.send_message(chat_id=chat[0], text=message_text))
+                    asyncio.run(bot.send_message(chat_id=chat[0], text=message_text))
 
     except docker.errors.NotFound:
         logging.error(f"Container '{container_name}' not found.")
@@ -98,17 +104,18 @@ def monitor_logs() -> None:
 
 if __name__ == '__main__':
     logging.info("Starting Telegram bot...")
-
+    logging.info(f"Allowed chat ids: {chat_list}")
+    print(chat_list)
+    print(type(chat_list[0]))
     application = ApplicationBuilder().token(bot_token).build()
-
-    application.add_handler(CommandHandler('start', start))
-    application.add_handler(CommandHandler('enable_messages', enable_messages))
-    application.add_handler(CommandHandler('disable_messages', disable_messages))
-    application.add_handler(CommandHandler('stop', stop))
+    application.add_handler(CommandHandler('start', start, filters= filters.Chat(chat_list)))
+    application.add_handler(CommandHandler('enable_messages', enable_messages, filters= filters.Chat(chat_list)))
+    application.add_handler(CommandHandler('disable_messages', disable_messages, filters= filters.Chat(chat_list)))
+    application.add_handler(CommandHandler('stop', stop, filters=filters.Chat(chat_list)))
+    application.add_handler(MessageHandler(None, callback=restrict))
 
     # Start log monitoring in a separate thread
     thread1 = Thread(target=monitor_logs, daemon=True)
     thread1.start()
     # Run bot polling in the main thread
     application.run_polling()
-

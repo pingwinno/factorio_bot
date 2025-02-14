9e1a232f6eabcf90cd3924e829689037f08407dd
max <pingwinno@gmail.com>
2025-02-14 03:21:23 +0200

    Add forward messages from tg to factorio

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import multiprocessing
 import os
 import sqlite3
 import time
-from threading import Thread
+from rcon.source import Client
 
 import docker
 from telegram import Update, Bot
@@ -15,6 +15,9 @@ from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, Messa
 # Load environment variables
 bot_token = os.environ['APIKEY']
 container_name = os.environ['CONTAINER_NAME']
+rcon_server = os.environ['RCON_SERVER']
+rcon_port = int(os.environ['RCON_PORT'])
+rcon_pwd = os.environ['RCON_PWD']
 
 chat_list = json.loads(os.environ['CHAT_LIST'])
 
@@ -28,10 +31,16 @@ settings_con = sqlite3.connect("db/settings.db", check_same_thread=False)
 settings_cur = settings_con.cursor()
 settings_cur.execute("CREATE TABLE IF NOT EXISTS chat_settings(chat_id NUMERIC PRIMARY KEY, messages_enabled BOOLEAN)")
 
+user_con = sqlite3.connect("db/user_settings.db", check_same_thread=False)
+user_cur = user_con.cursor()
+user_cur.execute("CREATE TABLE IF NOT EXISTS user_settings(user_id NUMERIC PRIMARY KEY, username TEXT, color TEXT)")
 # SQL Queries
 add_chat = "INSERT OR REPLACE INTO chat_settings VALUES(?, ?);"
 get_chats = "SELECT * FROM chat_settings;"
 delete_chat = "DELETE FROM chat_settings WHERE chat_id = ?;"
+
+add_user = "INSERT OR REPLACE INTO user_settings VALUES(?, ?, ?);"
+get_user = "SELECT username, color FROM user_settings WHERE user_id = ?;"
 
 # Configure logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
@@ -48,8 +57,8 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     chat_id = update.message.chat_id
     settings_cur.execute(add_chat, [chat_id, False])
     settings_con.commit()
-    await context.bot.send_message(chat_id=chat_id,
-                                   text="Chat added. Type /enable_messages to receive Factorio messages.")
+    await context.bot.send_message_to_tg(chat_id=chat_id,
+                                         text="Chat added. Type /enable_messages to receive Factorio messages.")
 
 
 async def stop(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -57,7 +66,7 @@ async def stop(update: Update, context: ContextTypes.DEFAULT_TYPE):
     chat_id = update.message.chat_id
     settings_cur.execute(delete_chat, [chat_id])
     settings_con.commit()
-    await context.bot.send_message(chat_id=chat_id, text="Chat deleted.")
+    await context.bot.send_message_to_tg(chat_id=chat_id, text="Chat deleted.")
 
 
 async def enable_messages(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -65,7 +74,7 @@ async def enable_messages(update: Update, context: ContextTypes.DEFAULT_TYPE):
     chat_id = update.message.chat_id
     settings_cur.execute(add_chat, [chat_id, True])
     settings_con.commit()
-    await context.bot.send_message(chat_id=chat_id, text="Messages enabled.")
+    await context.bot.send_message_to_tg(chat_id=chat_id, text="Messages enabled.")
 
 
 async def disable_messages(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -73,12 +82,12 @@ async def disable_messages(update: Update, context: ContextTypes.DEFAULT_TYPE):
     chat_id = update.message.chat_id
     settings_cur.execute(add_chat, [chat_id, False])
     settings_con.commit()
-    await context.bot.send_message(chat_id=chat_id, text="Messages disabled.")
+    await context.bot.send_message_to_tg(chat_id=chat_id, text="Messages disabled.")
 
 
 async def restart_server(update: Update, context: ContextTypes.DEFAULT_TYPE):
     logging.info("Received /restart_server command.")
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=f"Restarting...")
+    await context.bot.send_message_to_tg(chat_id=update.effective_chat.id, text=f"Restarting...")
     await context.bot.send_chat_action(chat_id=update.effective_chat.id, action=ChatAction.TYPING)
     container = client.containers.get(container_name)
     try:
@@ -86,16 +95,29 @@ async def restart_server(update: Update, context: ContextTypes.DEFAULT_TYPE):
         await asyncio.sleep(10)
     except docker.errors.NotFound as error:
         logging.error(f"Can't restart server '{error}'.")
-        await context.bot.send_message(chat_id=update.effective_chat.id,
-                                       text=f"Error during server restart: {error}")
+        await context.bot.send_message_to_tg(chat_id=update.effective_chat.id,
+                                             text=f"Error during server restart: {error}")
 
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=f"Server restarted. Status {container.status}")
+    await context.bot.send_message_to_tg(chat_id=update.effective_chat.id, text=f"Server restarted. Status {container.status}")
     stop_monitor_process()
     start_monitor_process()
 
+async def forward(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    message = update.message.text
+    user_id = update.message.from_user.id
+    user_metadata = user_cur.execute(get_user, [user_id]).fetchone()
+    user_name = user_metadata[0]
+    color = user_metadata[1]
+    send_message_to_factorio(f"{user_name}: {message}", color)
 
+async def set_user(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user_id = update.message.from_user.id
+    data = update.message.text.split(" ")
+    user_name = data[1]
+    color = data[2]
+    user_cur.execute(add_user, [user_id, user_name, color])
+    user_con.commit()
 
-# === CONTINUOUS LOG FILE MONITORING === #
 def monitor_logs() -> None:
     logging.info("Starting log monitoring...")
     try:
@@ -106,13 +128,12 @@ def monitor_logs() -> None:
             line = log.decode("utf-8").strip()
             logging.info(f"Log line: {line}")
 
-            message_text = ""
             if "[JOIN]" in line:
-                send_message(line.split("[JOIN]")[1].strip())
+                send_message_to_tg(line.split("[JOIN]")[1].strip())
             elif "[LEAVE]" in line:
-                send_message(line.split("[LEAVE]")[1].strip())
-            elif "[CHAT]" in line:
-                send_message(line, True)
+                send_message_to_tg(line.split("[LEAVE]")[1].strip())
+            elif "[CHAT]" in line and not "<server>" in line:
+                send_message_to_tg(line, True)
 
     except docker.errors.NotFound:
         logging.error(f"Container '{container_name}' not found.")
@@ -120,8 +141,8 @@ def monitor_logs() -> None:
         logging.error(f"Error in log monitoring: {e}")
 
 
-def send_message(message, is_chat=False):
-    logging.info(f"Send message: {message}")
+def send_message_to_tg(message, is_chat=False):
+    logging.info(f"Send message to TG: {message}")
     chats = settings_cur.execute(get_chats).fetchall()
     for chat in chats:
         bot = Bot(token=bot_token)
@@ -129,6 +150,12 @@ def send_message(message, is_chat=False):
         if is_chat and chat[1] == 0:
             return
         asyncio.run(bot.send_message(chat_id=chat[0], text=message))
+
+def send_message_to_factorio(message, color=None):
+    logging.info(f"Send message to Factorio: {message}")
+
+    with Client(rcon_server, rcon_port, passwd=rcon_pwd) as client:
+        client.run(f"[color={color}]{message}[/color]")
 
 def start_monitor_process():
     global monitor_thread
@@ -145,10 +172,12 @@ if __name__ == '__main__':
     print(type(chat_list[0]))
     application = ApplicationBuilder().token(bot_token).build()
     application.add_handler(CommandHandler('start', start, filters=filters.Chat(chat_list)))
+    application.add_handler(CommandHandler('set_user', set_user, filters=filters.Chat(chat_list)))
     application.add_handler(CommandHandler('restart_server', restart_server, filters=filters.Chat(chat_list)))
     application.add_handler(CommandHandler('enable_messages', enable_messages, filters=filters.Chat(chat_list)))
     application.add_handler(CommandHandler('disable_messages', disable_messages, filters=filters.Chat(chat_list)))
     application.add_handler(CommandHandler('stop', stop, filters=filters.Chat(chat_list)))
+    application.add_handler(MessageHandler(filters=filters.Chat(chat_list), callback=forward))
     application.add_handler(MessageHandler(None, callback=restrict))
 
     # Start log monitoring in a separate thread
